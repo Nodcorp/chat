2a573f8a549aee615ba443015be9605afb9d5f70
Nodlock <nodlockincorporated@gmail.com>
2025-11-04 22:40:39 +0000

    Update keepAlive.js

--- chat-app/utils/keepAlive.js
+++ chat-app/utils/keepAlive.js
@@ -1,16 +1,1 @@
-import fetch from 'node-fetch';
-import dotenv from 'dotenv';
-dotenv.config();
 
-const target = process.env.PING_URL;
-if (target) {
-  console.log(`Keep-alive ping enabled for ${target}`);
-  setInterval(async () => {
-    try {
-      const res = await fetch(`${target}/ping`);
-      console.log(`Pinged ${target}/ping - ${res.status}`);
-    } catch (err) {
-      console.error('Ping failed:', err.message);
-    }
-  }, 12 * 60 * 1000);
-}
